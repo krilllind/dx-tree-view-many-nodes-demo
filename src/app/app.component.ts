f2449207fd76e03267072805bd71fce85e5dfe6d
Kristoffer Lindström <kristoffer.lindstrom@bizviewsystems.com>
2019-08-21 09:34:03 +0200

    Creating initial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dx-tree-view-many-nodes-demo';
+  public datasource: { id: number, parentId: number; expanded: boolean; text: string; hasItems: boolean }[];
+  public size = 1000;
+  public depth = 3;
+  public expanded = false;
+  public log = false;
+
+  constructor() { }
+
+  public getData(): void {
+    this.datasource = new Array(this.size)
+      .fill(0)
+      .map((_, i) =>  {
+        let parentId = -1;
+        const hasItems: boolean = !((i + 1) % this.depth === 0);
+
+        if (i === 0 || i % this.depth === 0) {
+          parentId = -1;
+        } else {
+          parentId = i - 1;
+        }
+
+        return ({ id: i, parentId, text: `node-${i}`, expanded: this.expanded, hasItems });
+      });
+
+    if (this.log) {
+      console.table(this.datasource);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { DxTreeViewModule } from 'devextreme-angular';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    DxTreeViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
